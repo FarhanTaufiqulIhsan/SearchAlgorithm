a5194caa46b054cab65ea8f554b356d23f23edf0
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-10-26 16:17:14 +0700

    Adding method to accept array elements

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -21,6 +21,17 @@
                 else
                     Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements. \n");
             }
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" Enter array elements ");
+            Console.WriteLine("----------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<"+(i+1)+">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
 
     }
