6a1508cfd6694e9b4b9324e1b0b68f29923879d7
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-10-26 16:12:33 +0700

    Creating input method

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -8,5 +8,20 @@
         int n;
         //Get the number of elements to store in the array
         int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of the element in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements. \n");
+            }
+        }
+
     }
 }
